c73b2dc0414181e11ecf9dd31df5b770d639d5c8
Ravindra Sampath Kumara <wrskumara0108@gmail.com>
2025-02-28 23:19:23 +0530

    Create Header and Footer page

---
+++ frontend-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Footer.css'; // Import the CSS file for styling
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">© 2024 LMS. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './css/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-title">LMS Dashboard</h1>
+      
+    </header>
+  );
+}
+
+export default Header;
